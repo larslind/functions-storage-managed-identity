4ec37add5b74612eafde9c52fc709ade774ea8c8
Lars Lindstedt <lars.lindstedt@microsoft.com>
2020-06-18 01:06:27 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,17 @@
 variable "basename" {
   type        = string
   description = "The base name for all resources"
+  default     = "rftappserverdemo1"
 }
 
 variable "resource_group_name" {
   type        = string
   description = "rg name. Use only lowercase letters and numbers"
-  default     = "functions-storage-managed-identity"
+  default     = "rftappserverdemo1"
 }
 
 variable "location" {
   type        = string
   description = "Azure region where to create resources."
-  default     = "West US"
+  default     = "East US 2"
 }
